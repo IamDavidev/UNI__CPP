b3d2d997703ae7d7fb01f831d4f684a1e6ccc805
L-David-26 <davidlez2mgk@gmail.com>
2021-10-27 21:06:11 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// main c++
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout << "cual es tu nombre ";
+    cin >> name;
+    cout << "hola " << name << " bienvenido a la programacion !!!" << endl;
+}
